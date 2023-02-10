545a054f4a1a96590e063d058d2e31ca4a493784
ryeeun <dmsdl7531@naver.com>
2023-02-10 15:17:52 +0900

    chore : 로그인 관련 파일 및 route 삭제

--- fe/src/App.jsx
+++ fe/src/App.jsx
@@ -3,15 +3,10 @@ import { Routes, Route } from 'react-router-dom';
 import './App.css';
 
 import Main from './Main/Main';
-import Login from './Login/Login';
-import SignUp from './SignUp/SignUp';
 import Write from './Write/Write';
 import Manager from './Manager/Manager';
-import FindID from './FindID/FindID';
 import Layout from './Layout/Layout';
 import MyPage from './MyPage/MyPage';
-import FindPassword from './FindPassword/FindPassword';
-import NewPassword from './NewPassword/NewPassword';
 import Tech from './Tech/Tech';
 import Post from './Post/Post';
 import UserList from './Manager/components/UserList';
@@ -24,13 +19,8 @@ function App() {
     <Routes>
       <Route element={<Layout />}>
         <Route path="/" element={<Main />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
         <Route path="/write" element={<Write />} />
-        <Route path="/findid" element={<FindID />} />
         <Route path="/mypage" element={<MyPage />} />
-        <Route path="/findpassword" element={<FindPassword />} />
-        <Route path="/newpassword" element={<NewPassword />} />
         <Route path="/posts" element={<Tech />}>
           <Route path=":category" element={<Tech />} />
         </Route>
