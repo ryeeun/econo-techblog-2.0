e5517bd1bc85ba12044066f91c1a289e1ec0d180
ryeeun <dmsdl7531@naver.com>
2023-02-27 15:56:26 +0900

    feat : header 검색창 페이지 이동 로직 추가

--- fe/src/Header/components/Search.jsx
+++ fe/src/Header/components/Search.jsx
@@ -1,12 +1,31 @@
 import React, { useState } from 'react';
 // eslint-disable-next-line import/no-extraneous-dependencies
+import { useNavigate } from 'react-router-dom';
 
 import searchButton from '../img/search_button.png';
 import './Search.css';
 
 function Search() {
+  const [text, setText] = useState('');
+  const navigate = useNavigate();
   const [isSearchBarOn, setIsSearchBarOn] = useState(false);
+  const onChange = (e) => {
+    setText(() => e.target.value);
+  };
+
+  const onSubmit = () => {
+    navigate('/search', { state: { value: text } });
+  };
+
+  const handleOnKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      onSubmit();
+    }
+  };
+
   const onClick = () => {
+    if (text !== '') onSubmit();
+    setText('');
     setIsSearchBarOn(() => !isSearchBarOn);
   };
 
@@ -15,9 +34,12 @@ function Search() {
       {isSearchBarOn && (
         <input
           className="search-input"
+          onChange={onChange}
+          value={text}
+          onKeyDown={handleOnKeyPress}
         />
       )}
-      <button className="search-button" type="button" onClick={onClick}>
+      <button className="search-button" type="submit" onClick={onClick}>
         <img
           className="search-button-img"
           alt="search-button"
