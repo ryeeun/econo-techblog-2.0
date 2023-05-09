28bc994b5a37ce5f9c8345614565d5addeca530c
ryeeun <dmsdl7531@naver.com>
2023-05-09 17:47:26 +0900

    feat : 글쓰기 input 여러 줄 처리

--- fe/src/Write/TitleInput.jsx
+++ fe/src/Write/TitleInput.jsx
@@ -1,16 +1,21 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import React from 'react';
+import React, { useRef } from 'react';
 import PropTypes from 'prop-types';
 
 import './css/TitleInput.css';
 
 function TitleInput({ setTitle }) {
+  const titleInput = useRef();
+  const titleBox = useRef();
   const onTitleChange = (e) => {
     setTitle(e.target.value);
+    titleBox.current.style.height = '3vw';
+    titleBox.current.style.height = `${titleInput.current.scrollHeight}px`;
+    console.log(titleInput.current.scrollHeight);
   };
   return (
-    <div className="title-input-container">
-      <input className="title-input" placeholder="제목을 입력하세요..." onChange={onTitleChange} />
+    <div ref={titleBox} className="title-input-container">
+      <textarea ref={titleInput} className="title-input" placeholder="제목을 입력하세요..." rows={1} onChange={onTitleChange} />
     </div>
   );
 }
