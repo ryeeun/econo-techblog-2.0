9bfa2314aaf0e4a4184e554dc98470bbbbb650ff
ryeeun <dmsdl7531@naver.com>
2023-06-28 19:26:30 +0900

    feat : category 관련 로직 수정

--- fe/src/Write/CategorySelectBox.jsx
+++ fe/src/Write/CategorySelectBox.jsx
@@ -2,49 +2,57 @@
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 import './css/CategorySelectBox.css';
 
-function CategorySelectBox({ categoryNum, setCategoryNum }) {
+function CategorySelectBox({ categoryName, setCategoryName }) {
   const [isOpen, setOpen] = useState(false);
-  const [categoryName, setCategoryName] = useState('게시판을 선택하세요');
+
+  const findCategory = (e) => {
+    if (e === 1) return 'TECH';
+    if (e === 2) return 'CULTURE';
+    return 'TROUBLE SHOOTING';
+  };
+
   const onClick = () => {
     setOpen(!isOpen);
   };
 
-  const findCategory = (e) => {
-    if (e === 0) return '게시판을 선택하세요';
-    if (e === 1) return 'Tech';
-    if (e === 2) return 'Culture';
-    return 'Trouble Shooting';
-  };
-
   const onSelect = (e) => {
-    setCategoryNum(e.target.value);
+    setCategoryName(findCategory(e.target.value));
   };
 
-  useEffect(() => {
-    setCategoryName(findCategory(categoryNum));
-  }, [categoryNum]);
   return (
     <div className="category-select-box" type="button" onClick={onClick}>
-      <span className={categoryNum !== 0 ? 'category-select-box-text selected' : 'category-select-box-text'}>{categoryName}</span>
+      {categoryName === '' ? (
+        <span className="category-select-box-text">게시판을 선택하세요</span>
+      ) : (
+        <span className="category-select-box-text selected">
+          {categoryName}
+        </span>
+      )}
       {isOpen && (
-      <ul className="category-select-list">
-        <li className="category-select-item" onClick={onSelect} value="1">Tech</li>
-        <li className="category-select-item" onClick={onSelect} value="2">Culture</li>
-        <li className="category-select-item" onClick={onSelect} value="3">Trouble Shooting</li>
-      </ul>
+        <ul className="category-select-list">
+          <li className="category-select-item" onClick={onSelect} value="1">
+            TECH
+          </li>
+          <li className="category-select-item" onClick={onSelect} value="2">
+            CULTURE
+          </li>
+          <li className="category-select-item" onClick={onSelect} value="3">
+            TROUBLE SHOOTING
+          </li>
+        </ul>
       )}
     </div>
   );
 }
 
 CategorySelectBox.propTypes = {
-  categoryNum: PropTypes.number.isRequired,
-  setCategoryNum: PropTypes.func.isRequired,
+  categoryName: PropTypes.string.isRequired,
+  setCategoryName: PropTypes.func.isRequired,
 };
 
 export default CategorySelectBox;
