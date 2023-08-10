0dcda20993a05dc55266846c554dab03b998e878
ryeeun <dmsdl7531@naver.com>
2023-08-10 15:48:35 +0900

    feat : 대댓글 레이아웃 구현

--- fe/src/Post/components/Comment.jsx
+++ fe/src/Post/components/Comment.jsx
@@ -1,25 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import PropTypes from 'prop-types';
 import noImg from '../img/no_img.png';
 
 import '../css/Comment.css';
+import CommentWrite from './CommentWrite';
 
-const Comment = function ({ name, desc }) {
+const Comment = function ({ name, content }) {
+  const [isOpen, setOpen] = useState(false);
+
+  const onClickReply = () => {
+    setOpen((cur) => !cur);
+  };
   return (
     <div className="comment">
-      <img src={noImg} alt="no-img" className="comment__img" />
-      <div className="comment-info">
-        <span className="comment-info__writer">{name}</span>
-        <span className="comment-info__desc">{desc}</span>
+      <div className="comment-box">
+        <img src={noImg} alt="no-img" className="comment-box__img" />
+        <div className="comment-box-info">
+          <span className="comment-box-info__writer">{name}</span>
+          <span className="comment-box-info__desc">{content}</span>
+          <button
+            type="button"
+            className="comment-box-info-reply__btn"
+            onClick={onClickReply}
+          >
+            {isOpen ? '답글 취소' : '답글'}
+          </button>
+        </div>
       </div>
+      {isOpen && <CommentWrite />}
     </div>
   );
 };
 
 Comment.propTypes = {
   name: PropTypes.string.isRequired,
-  desc: PropTypes.string.isRequired,
+  content: PropTypes.string.isRequired,
 };
 
 export default Comment;
--- fe/src/Post/components/CommentList.jsx
+++ fe/src/Post/components/CommentList.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 import '../css/CommentList.css';
@@ -10,26 +10,31 @@ const result = [
   {
     id: 1,
     name: '김수민',
-    desc: '안녕하세요 궁금한점이 있습니다! 만약에 사용자가 빌린 차를 세차한 것이 아니라 구글링이나 다른 자동차를 세차한 이미지를 올린다면 이건 어떤 방식으로 걸러내는 건가요?',
+    content:
+      '안녕하세요 궁금한점이 있습니다! 만약에 사용자가 빌린 차를 세차한 것이 아니라 구글링이나 다른 자동차를 세차한 이미지를 올린다면 이건 어떤 방식으로 걸러내는 건가요?',
   },
   {
     id: 2,
     name: '김수민',
-    desc: '안녕하세요 궁금한점이 있습니다! 만약에 사용자가 빌린 차를 세차한 것이 아니라 구글링이나 다른 자동차를 세차한 이미지를 올린다면 이건 어떤 방식으로 걸러내는 건가요?',
+    content:
+      '안녕하세요 궁금한점이 있습니다! 만약에 사용자가 빌린 차를 세차한 것이 아니라 구글링이나 다른 자동차를 세차한 이미지를 올린다면 이건 어떤 방식으로 걸러내는 건가요?',
   },
   {
     id: 3,
     name: '김수민',
-    desc: '안녕하세요 궁금한점이 있습니다! 만약에 사용자가 빌린 차를 세차한 것이 아니라 구글링이나 다른 자동차를 세차한 이미지를 올린다면 이건 어떤 방식으로 걸러내는 건가요?',
+    content:
+      '안녕하세요 궁금한점이 있습니다! 만약에 사용자가 빌린 차를 세차한 것이 아니라 구글링이나 다른 자동차를 세차한 이미지를 올린다면 이건 어떤 방식으로 걸러내는 건가요?',
   },
 ];
 
 const CommentList = function () {
+  const [comments, setComments] = useState([]);
   useEffect(() => {
     axios
       .get(`${process.env.REACT_APP_TECONO_API_URL}/boards/1/comments`)
       .then((response) => {
         console.log(response);
+        setComments(response.data.data.comments);
       })
       .catch((error) => {
         console.log('error', error);
@@ -39,8 +44,21 @@ const CommentList = function () {
     <div className="comment-list">
       <span className="comment-list__num">{`댓글 ${result.length}개`}</span>
       <CommentWrite />
-      {result.map((comment) => (
-        <Comment key={comment.id} name={comment.name} desc={comment.desc} />
+      {comments.map((comment) => (
+        <div>
+          <Comment key={comment.id} name="김수민" content={comment.content} />
+          {comment.reply.length > 0 && (
+            <div className="comment-list-reply">
+              {comment.reply.map((reply) => (
+                <Comment
+                  key={comment.id}
+                  name="김수민"
+                  content={reply.content}
+                />
+              ))}
+            </div>
+          )}
+        </div>
       ))}
     </div>
   );
